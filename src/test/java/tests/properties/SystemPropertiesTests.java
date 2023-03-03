329b1757ba25377af4f46bc6dbc1ac08dca30ddf
petr <panisimov@gmail.com>
2023-03-03 15:48:11 +0800

    one more parametr for Jenkiens

--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -6,6 +6,32 @@ import org.junit.jupiter.api.Test;
 public class SystemPropertiesTests {
 
     @Test
+    void someTest1() {
+        String browser = System.getProperty("browser");
+        System.out.println(browser); // null
+    }
+
+    @Test
+    void someTest2() {
+        System.setProperty("browser", "safari");
+        String browser = System.getProperty("browser");
+        System.out.println(browser); // safari
+    }
+
+    @Test
+    void someTest3() {
+        String browser = System.getProperty("browser", "opera");
+        System.out.println(browser); // opera
+    }
+
+    @Test
+    void someTest4() {
+        System.setProperty("browser", "safari");
+        String browser = System.getProperty("browser", "opera");
+        System.out.println(browser); // safari
+    }
+
+    @Test
     @Tag("test5")
     void someTest5() {
         String browser = System.getProperty("browser", "chrome");
@@ -15,11 +41,22 @@ public class SystemPropertiesTests {
         System.out.println(browser);
         System.out.println(version);
         System.out.println(browserSize);
+
+        /*
+           gradle clean test5
+                chrome
+                100
+                1920x1080
+           gradle clean test5 -Dbrowser=opera -Dversion=99 -DbrowserSize=300x300
+                opera
+                99
+                300x300
+         */
     }
 
     @Test
     void someTest6() {
         System.out.println("I want to say: " + System.getProperty("anyText"));
     }
-    
+}
 }
